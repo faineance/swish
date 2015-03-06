724e7d8d4f0c38179ea171e44275ed4ab2f5e964
faineance <faineance@users.noreply.github.com>
2015-03-06 21:23:12 +0000

    prompt text

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::{io, os, env, path, process};
 
+
 struct Job {
     process : process::Child,
     name : String
@@ -7,12 +8,30 @@ struct Job {
 struct HistoryItem {
     args : Vec<String>
 }
+fn prompt() -> String {
+    let mut prompt = String::new();
+    let pwd = Path::new(os::getenv("PWD").unwrap());
+    let homedir = Path::new(os::homedir().unwrap());
+
+    if pwd == homedir {
+        prompt.push_str("~")
+    } else {
+        prompt.push_str(pwd.as_str().unwrap());
+    }
+    prompt.push_str("$");
+    prompt
+
+    
+
+}
+
 
 fn main() {
     let mut jobs: Vec<Job> = vec![];
     let mut history: Vec<HistoryItem> = vec![];
-
+    println!("Swish");
     loop {
-        
+        let prompt = prompt();
+
     }
 }
